fe7e054d4e81efa74f70c5ab107cc8a8120e4a3e
areed98 <austinjreed89@gmail.com>
2022-12-15 23:03:59 -0500

    employee array finished

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const Intern = require('./lib/Intern');
 const employeeArrary = [];
 
 const createManager = () => {
+
+    console.log(`
+    -----------------------------
+    Fill out the manager position
+    -----------------------------
+    `);
     return inquirer
     .createPromptModule([
         {
@@ -77,4 +83,113 @@ const createManager = () => {
         employeeArrary.push(manager);
         console.log(manager);
     });
+};
+
+const createEmployee = () => {
+    console.log(`
+    --------------------------------
+    Fill out your employee positions
+    --------------------------------
+    `);
+
+    return inquirer
+        .createPromptModule([
+            {
+                type: "list",
+                name: "role",
+                message: "Please choose one of the following:",
+                choices: ["Engineer", "Intern"],
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the employee's name?",
+                validate: (employeeName) => {
+                    if (employeeName) {
+                        return true;
+                    } else {
+                        console.log('Please enter a name to continue.');
+                        return false;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the employee's ID.",
+                validate: (employeeID) => {
+                    if (isNaN(employeeID)) {
+                        console.log("Please enter a valid ID.");
+                    } else {
+                        return true;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the employee's email.",
+                validate: (email) => {
+                    valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("Please enter a proper email");
+                        return false;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Please enter the employee's github username",
+                when: (input) => input.role === "Engineer",
+                validate: (githubCheck) => {
+                    if (githubCheck) {
+                        return true;
+                    } else {
+                        console.log("Please enter a github username.");
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Does this employee attend school?",
+                when: (input) => input.role === "Intern",
+                validate: (schoolCheck) => {
+                    if (schoolCheck) {
+                        return true;
+                    } else {
+                        console.log("Please enter the school that the intern is attending.");
+                    }
+                },
+            },
+            {
+                type: "confirm",
+                name: "confirmEmployee",
+                message: "Would you like to add more employees?",
+                default: false,
+            },
+        ])
+        .then((employeeData) => {
+            let { name, id, email, role, github, school, confirmEmployee } = employeeData;
+            let employees;
+
+            if (role === "Engineer") {
+                employees = new Engineer(name, id, email, github);
+                console.log(employees);
+            } else if (role === "Intern") {
+                employees = new Intern(name, id, email, school);
+                console.log(employees);
+            }
+
+            employeeArrary.push(employees);
+
+            if (confirmEmployee) {
+                return createEmployee(employeeArrary);
+            } else {
+                return employeeArrary;
+            }
+        });
 };
